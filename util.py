c6c2dc3ef85b7a0f49a122b61597308cb4315129
victorli112 <victor.li.7802@gmail.com>
2024-04-29 12:31:09 -0400

    test

--- util.py
+++ util.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from sys import argv
+import requests
+import lxml
+import bs4
 
 # combine a list of xlsx files into one
 def combine_xlsx_files(output_file, files):
@@ -12,6 +15,14 @@ def combine_xlsx_files(output_file, files):
     df.to_excel(output_file, index=False)
     print("Length of combined file", len(df))
 
-if __name__ == "__main__":
-    files = argv[1:]
-    combine_xlsx_files("combined_wsj.xlsx", files)
+def test():
+    URL = "https://archive.is/AdIrq"
+    response = requests.get(URL)
+    soup = bs4.BeautifulSoup(response.content, 'lxml')
+    print(soup.find('article').getText())
+    
+test()
+
+# if __name__ == "__main__":
+#     files = argv[1:]
+#     combine_xlsx_files("combined_wsj.xlsx", files)
